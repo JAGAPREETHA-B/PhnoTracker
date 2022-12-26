8c8b410ee36b4ba8f192465e36ad30c3f31cc2d4
JAGAPREETHA-B <108733892+JAGAPREETHA-B@users.noreply.github.com>
2022-12-26 18:26:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import phonenumbers
+import opencage
+import folium
+from testing import num
+
+from phonenumbers import geocoder
+cy_number=phonenumbers.parse(num, "CH")
+location=geocoder.description_for_number(cy_number, "en")
+print(location)
+
+from phonenumbers import carrier
+service_num=phonenumbers.parse(num,"RO")
+print(carrier.name_for_number(service_num, "en"))
+
+from opencage.geocoder import OpenCageGeocode
+key= '32cb7763c2314adfa924d70a6c27eba2'
+
+geocoder = OpenCageGeocode(key)
+query = str(location)
+results = geocoder.geocode(query)
+#print(results)
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+print(lat,lng)
+
+myMap=folium.Map(location=[lat, lng], zoom_start=9)
+folium.Marker([lat, lng], popup=location).add_to(myMap)
+
+myMap.save("mytracker.html")
+
